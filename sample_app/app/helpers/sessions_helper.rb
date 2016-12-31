8f5be486b89cce49d44b7212965b9dad90d5387c
uyorum <uyorum.pub@gmail.com>
2016-12-31 19:32:08 +0900

    8.2.2 Added current_user method to get current user_id

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -4,4 +4,9 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 現在ログインしているユーザーを返す (ユーザーがログイン中の場合のみ)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
