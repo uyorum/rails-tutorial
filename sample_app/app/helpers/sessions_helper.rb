f13ef60b2e8b5d37edddd4868035e469d5616ca3
uyorum <uyorum.pub@gmail.com>
2017-01-03 17:32:58 +0900

    8.4.4 Fix a bug
    * logout in already logged-out browser returns error

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -39,7 +39,7 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
-    forget(current_user)
+    forget(current_user) if logged_in?
     session.delete(:user_id)
     @current_user = nil
   end
