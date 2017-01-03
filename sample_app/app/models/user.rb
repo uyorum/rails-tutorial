24696874c423a3c7b627280e5c1978326c6f46d9
uyorum <uyorum.pub@gmail.com>
2017-01-03 18:45:47 +0900

    8.6.1 Define class methods using 'self' prefix

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -10,14 +10,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   # 与えられた文字列のハッシュを返す
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
              BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # ランダムなトークンを返す
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
