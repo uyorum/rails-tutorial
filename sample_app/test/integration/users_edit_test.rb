d3b92aefb7ccff709382b92a537fedd7a8c796bd
uyorum <uyorum.pub@gmail.com>
2017-01-08 18:02:14 +0900

    9.1.4 Update users controller to edit user profile successfully

--- sample_app/test/integration/users_edit_test.rb
+++ sample_app/test/integration/users_edit_test.rb
@@ -6,7 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "unsuccessfull edit" do
+  test "unsuccessful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: "",
@@ -14,5 +14,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     password: "foo",
                                     password_confirmation: "bar" }
     assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password: "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
