04429807c187bcf792bfea1f5dc023b8b1616919
uyorum <uyorum.pub@gmail.com>
2017-01-14 15:35:26 +0900

    10.1.3 Update to be able to activate an account

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -29,9 +29,10 @@ class User < ActiveRecord::Base
   end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_token.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーログインを破棄する
