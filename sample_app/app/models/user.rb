6317a08f2c0673e2ab57a149aeb53e80573c0112
uyorum <uyorum.pub@gmail.com>
2017-01-15 20:38:42 +0900

    10.5.3 update_columns from multiple 'update_attribute's

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -42,8 +42,7 @@ class User < ActiveRecord::Base
 
   # アカウントを有効にする
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
@@ -54,8 +53,8 @@ class User < ActiveRecord::Base
   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   # パスワード再設定のメールを送信する
